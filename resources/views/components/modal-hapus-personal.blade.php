4c280b87abab3e35eb444c2366a6ee3fe3923720
martinrahmanhakim <martinrahmanhakim@student.telkomuniversity.ac.id>
2023-12-14 23:01:00 +0700

    menambahkan modal hapus dan done

---
+++ resources/views/components/modal-hapus-personal.blade.php
@@ -0,0 +1,25 @@
+<div id="hs-slide-down-animation-modal4" class="hs-overlay hs-overlay-backdrop-open:bg-secondary hs-overlay-backdrop-open:bg-opacity-50 hidden w-full h-full fixed top-0 start-0 z-[60] overflow-x-hidden overflow-y-auto">
+    <div class="hs-overlay-open:opacity-100 hs-overlay-open:duration-500 opacity-0 transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto mt-64">
+      <div class="flex flex-col bg-card  shadow-sm rounded-xl">
+        <div class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700">
+            <h3 class="font-bold text-gray-800 dark:text-white">
+              Delete Schedule
+            </h3>
+
+          </div>
+          <div class="p-4 overflow-y-auto">
+            <p class="mt-1 text-gray-800 dark:text-gray-400">
+              Jika anda menghapus schedul Lorem maka tidak akan bisa dipulihkan, apakah kamu yakin ingin menghapus schedule ini ?
+            </p>
+          </div>
+          <div class="flex justify-center items-center gap-x-2 py-3 px-4 ">
+            <button type="button" class="py-2 px-8 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border  text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-overlay="#hs-slide-down-animation-modal4">
+              Hapus
+            </button>
+            <button type="button" class="py-2 px-8 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg -transparent bg-white text-[#000] bg-blue-600 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-overlay="#hs-slide-down-animation-modal4">
+              Batal
+            </button>
+          </div>
+      </div>
+   </div>
+</div>
